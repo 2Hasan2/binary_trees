adf8311e4796e2ea2814fee664624992c6103665
2Hasan2 <h01554176846@gmail.com>
2024-03-27 15:40:17 +0200

    2024-03-27 03:40:17 PM

---
+++ 2-binary_tree_insert_right.c
@@ -0,0 +1,31 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_insert_right - Inserts a node as the right child of another node
+ * @parent: Pointer to the parent node
+ * @value: Value to be stored in the new node
+ *
+ * Return: Pointer to the newly created node, or NULL on failure
+ */
+binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value)
+{
+    binary_tree_t *new_node;
+
+    if (parent == NULL)
+        return (NULL);
+
+    new_node = binary_tree_node(parent, value);
+    if (new_node == NULL)
+        return (NULL);
+
+    if (parent->right != NULL)
+    {
+        new_node->right = parent->right;
+        parent->right->parent = new_node;
+    }
+
+    parent->right = new_node;
+    new_node->parent = parent;
+
+    return (new_node);
+}
--- compile.sh
+++ compile.sh
@@ -1,10 +1,16 @@
 #!/bin/bash
 
-# Check if the correct number of arguments is provided
-if [ "$#" -ne 2 ]; then
-    echo "Usage: $0 <main_file> <binary_tree_node_file>"
+# Check if at least one .c file is present
+if ! ls *.c &>/dev/null; then
+    echo "No .c files found in the directory."
     exit 1
 fi
 
-# Compile the program
-gcc -Wall -Wextra -Werror -pedantic binary_tree_print.c "$1" "$2" -o out
+# Check if a number argument is provided
+if [ "$#" -ne 1 ]; then
+    echo "Usage: $0 <number>"
+    exit 1
+fi
+
+# Compile the program excluding files ending with '{number}-main.c'
+gcc -Wall -Wextra -Werror -pedantic $(ls *.c | grep -v "${1}-main.c") -o "${1}-main"
