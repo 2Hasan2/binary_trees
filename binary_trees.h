4c693b5e53074c81f4779556f35ee0a354bf446d
2Hasan2 <h01554176846@gmail.com>
2024-03-27 15:29:23 +0200

    2024-03-27 03:29:23 PM

--- binary_trees.h
+++ binary_trees.h
@@ -32,4 +32,8 @@ void binary_tree_print(const binary_tree_t *);
 
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value);
 
+
+
+binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value);
+
 #endif /* _BINARY_TREES_H_ */
