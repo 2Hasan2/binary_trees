586272762fafebf394f5321bc51601244b68f5b5
2Hasan2 <h01554176846@gmail.com>
2024-03-29 02:37:44 +0200

    betty

--- 10-binary_tree_depth.c
+++ 10-binary_tree_depth.c
@@ -6,15 +6,15 @@
  */
 size_t binary_tree_depth(const binary_tree_t *tree)
 {
-    size_t depth = 0;
+	size_t depth = 0;
 
-    if (!tree)
-        return (0);
+	if (!tree)
+		return (0);
 
-    while (tree->parent)
-    {
-        depth++;
-        tree = tree->parent;
-    }
-    return (depth);
+	while (tree->parent)
+	{
+		depth++;
+		tree = tree->parent;
+	}
+	return (depth);
 }
