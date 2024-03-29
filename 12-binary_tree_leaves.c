c06f6a7ebb33a43954f5a89142d8867e2a3b3b5d
2Hasan2 <h01554176846@gmail.com>
2024-03-29 02:37:26 +0200

    betty

--- 12-binary_tree_leaves.c
+++ 12-binary_tree_leaves.c
@@ -6,16 +6,16 @@
  */
 size_t binary_tree_leaves(const binary_tree_t *tree)
 {
-    size_t leaves = 0;
+	size_t leaves = 0;
 
-    if (!tree)
-        return (0);
+	if (!tree)
+		return (0);
 
-    if (!tree->left && !tree->right)
-        return (1);
+	if (!tree->left && !tree->right)
+		return (1);
 
-    leaves += binary_tree_leaves(tree->left);
-    leaves += binary_tree_leaves(tree->right);
+	leaves += binary_tree_leaves(tree->left);
+	leaves += binary_tree_leaves(tree->right);
 
-    return (leaves);
+	return (leaves);
 }
